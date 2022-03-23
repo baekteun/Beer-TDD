a4d9e907f0cf035c4e5cd95c7f4d9fb619dd26fd
baegteun <baegteun@gmail.com>
2022-03-23 14:26:49 +0900

    :sparkles: :: Repository Assembly

--- Service/Sources/Assembly/RepositoryAssembly.swift
+++ Service/Sources/Assembly/RepositoryAssembly.swift
@@ -2,7 +2,9 @@ import Swinject
 
 public final class RepositoryAssembly: Assembly {
     public func assemble(container: Container) {
-        
+        container.register(BeerRepository.self) { r in
+            return DefaultBeerRepository()
+        }.inObjectScope(.container)
     }
     public init(){}
 }
