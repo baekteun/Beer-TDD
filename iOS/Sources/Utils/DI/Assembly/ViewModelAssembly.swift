90d1a7f887ca0988feb31af4141bca35484b1378
baegteun <baegteun@gmail.com>
2022-03-23 14:28:28 +0900

    :sparkles: :: ViewModel Assembly

--- iOS/Sources/Utils/DI/Assembly/ViewModelAssembly.swift
+++ iOS/Sources/Utils/DI/Assembly/ViewModelAssembly.swift
@@ -1,10 +1,13 @@
 import Swinject
+import Service
 
 final class ViewModelAssembly: Assembly {
     func assemble(container: Container) {
         // MARK: - BeerList
         container.register(BeerListViewModel.self) { r in
-            BeerListViewModel()
+            BeerListViewModel(
+                fetchBeerListUseCase: r.resolve(FetchBeerListUseCase.self)!
+            )
         }.inObjectScope(.container)
         
     }
