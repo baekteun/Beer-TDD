ad86c3d8cc4f4094a310fc547dbd457f6f226b1f
baegteun <baegteun@gmail.com>
2022-03-23 14:26:59 +0900

    :sparkles: :: UseCase Assembly

--- Service/Sources/Assembly/UseCaseAssembly.swift
+++ Service/Sources/Assembly/UseCaseAssembly.swift
@@ -2,7 +2,11 @@ import Swinject
 
 public final class UseCaseAssembly: Assembly {
     public func assemble(container: Container) {
-        
+        container.register(FetchBeerListUseCase.self) { r in
+            return FetchBeerListUseCase(
+                beerRepository: r.resolve(BeerRepository.self)!
+            )
+        }
     }
     public init() {}
 }
