b37c067da7cfe816f863a652c0d11e59f1333576
baegteun <baegteun@gmail.com>
2022-03-23 18:21:51 +0900

    :sparkles: :: Remote Request searchBeer

--- Service/Sources/Data/Remote/DataSource/BeerRemote.swift
+++ Service/Sources/Data/Remote/DataSource/BeerRemote.swift
@@ -9,4 +9,12 @@ final class BeerRemote: BaseRemote<BeerAPI> {
             .map { $0.map { $0.toDomain() } }
             .eraseToAnyPublisher()
     }
+    func searchBeer(id: Int, isTest: Bool) -> AnyPublisher<Beer, Error>{
+        request(.searchBeer(id: id), isTest: isTest)
+            .map(\.data)
+            .decode(type: [BeerDTO].self, decoder: decoder)
+            .map { $0.first.map { $0.toDomain()} }
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
 }
