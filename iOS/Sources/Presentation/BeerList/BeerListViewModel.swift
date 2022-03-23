1ca09a8ae39b17e1c72faa8d60ca93d0abacbaeb
baegteun <baegteun@gmail.com>
2022-03-23 14:29:48 +0900

    :sparkles: :: BeerList ViewModel

--- iOS/Sources/Presentation/BeerList/BeerListViewModel.swift
+++ iOS/Sources/Presentation/BeerList/BeerListViewModel.swift
@@ -1,6 +1,39 @@
 import Combine
+import Service
 
-final class BeerListViewModel: ObservableObject {
+final class BeerListViewModel: BaseViewModel {
     // MARK: - Properties
+    @Published var beerList: [Beer] = []
+    private let fetchBeerListUseCase: FetchBeerListUseCase
+    var page: Int = 1
+    let size = 20
     
+    init(
+        fetchBeerListUseCase: FetchBeerListUseCase
+    ) {
+        self.fetchBeerListUseCase = fetchBeerListUseCase
+        super.init()
+    }
+     
+    enum Input {
+        case onAppear
+    }
+    
+    func apply(_ input: Input) {
+        switch input {
+        case .onAppear:
+            onAppear()
+        }
+    }
+    
+    // MARK: - Method
+    private func onAppear() {
+        addCancellable(publisher: fetchBeerListUseCase.execute(page: page, size: size)) { [weak self] value in
+            self?.beerList = value
+        } onReceiveFailure: { [weak self] err in
+            self?.isErrorOccurred = true
+            self?.errorMessage = err.localizedDescription
+        }
+
+    }
 }
--- iOS/Tests/ViewModel/BeetListViewModelSpec.swift
+++ iOS/Tests/ViewModel/BeetListViewModelSpec.swift
@@ -1,9 +1,35 @@
 import XCTest
 import Quick
+import Nimble
+@testable import Service
 @testable import Beer
 
 final class BeetListViewModelSpec: QuickSpec {
     override func spec() {
-        
+        var beerRepository: BeerRepository!
+        var fetchBeerListUseCase: FetchBeerListUseCase!
+        var vm: BeerListViewModel!
+        describe("View가 로드되고") {
+            beforeEach {
+                beerRepository = MockBeerRepository()
+                fetchBeerListUseCase = FetchBeerListUseCase(
+                    beerRepository: beerRepository
+                )
+                vm = BeerListViewModel(
+                    fetchBeerListUseCase: fetchBeerListUseCase
+                )
+                vm.apply(.onAppear)
+            }
+            context("데이터(Mock)가 fetch되면") {
+                
+                it("beerList의 카운터가 0보다 크다") {
+                    expect(vm.beerList.count).toEventually(beGreaterThan(0), timeout: .seconds(3))
+                }
+                it("beerList의 카운터가 size(20)만큼의 count를 가진다") {
+                    expect(vm.beerList.count).toEventually(equal(20), timeout: .seconds(3))
+                }
+            }
+            
+        }
     }
 }
