b429ab925e888a23348b7f3a07390b008756736e
baegteun <baegteun@gmail.com>
2022-03-23 22:38:17 +0900

    :sparkles: :: SearchBeer

--- iOS/Sources/Presentation/SearchBeer/SearchBeerViewModel.swift
+++ iOS/Sources/Presentation/SearchBeer/SearchBeerViewModel.swift
@@ -3,6 +3,9 @@ import Service
 
 final class SearchBeerViewModel: BaseViewModel {
     // MARK: - Properties
+    @Published var beer: Beer = Beer()
+    @Published var text: String = ""
+    
     private let searchBeerUseCase: SearchBeerUseCase
     
     init(
@@ -10,5 +13,28 @@ final class SearchBeerViewModel: BaseViewModel {
     ) {
         self.searchBeerUseCase = searchBeerUseCase
         super.init()
+    }
+    
+    enum Input {
+        case search
+    }
+    
+    func apply(_ input: Input) {
+        switch input {
+        case .search:
+            search(id: Int(text) ?? 0)
+        }
+    }
+}
+
+private extension SearchBeerViewModel {
+    func search(id: Int) {
+        addCancellable(publisher: searchBeerUseCase.execute(id: id)) { [weak self] value in
+            self?.beer = value
+        } onReceiveFailure: { [weak self] err in
+            self?.isErrorOccurred = true
+            self?.errorMessage = err.localizedDescription
+        }
+
     }
 }
--- iOS/Tests/ViewModel/SearchBeerViewModelSpec.swift
+++ iOS/Tests/ViewModel/SearchBeerViewModelSpec.swift
@@ -2,9 +2,36 @@
 import XCTest
 import Quick
 import Nimble
+@testable import Beer
+@testable import Service
 
 class SearchBeerViewModelSpec: QuickSpec {
     override func spec() {
-        
+        var beerRepository: BeerRepository!
+        var searchBeerUseCase: SearchBeerUseCase!
+        var vm: SearchBeerViewModel!
+        describe("View가 로드되고") {
+            beforeEach {
+                beerRepository = MockBeerRepository()
+                searchBeerUseCase = SearchBeerUseCase(beerRepository: beerRepository)
+                vm = SearchBeerViewModel(searchBeerUseCase: searchBeerUseCase)
+            }
+            context("사용자가 id: 2로 검색을 하면") {
+                beforeEach {
+                    vm.text = "2"
+                    vm.apply(.search)
+                }
+                
+                it("id가 2인 beer를 검색하고 viewModel의 beer가 변경된다") {
+                    let maybe = Beer(
+                        id: 2,
+                        name: "Trashy Blonde",
+                        description: "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.",
+                        imageUrl: "https://images.punkapi.com/v2/2.png"
+                    )
+                    expect(vm.beer).toEventually(equal(maybe), timeout: .seconds(5))
+                }
+            }
+        }
     }
 }
