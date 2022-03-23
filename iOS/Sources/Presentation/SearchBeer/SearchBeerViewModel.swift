686a161ba26c1756b66f9550bbf328f190c98c23
baegteun <baegteun@gmail.com>
2022-03-23 21:43:02 +0900

    :sparkles: :: SearchBeer DI

--- iOS/Sources/Presentation/SearchBeer/SearchBeerViewModel.swift
+++ iOS/Sources/Presentation/SearchBeer/SearchBeerViewModel.swift
@@ -1,5 +1,14 @@
 import Combine
+import Service
 
 final class SearchBeerViewModel: BaseViewModel {
+    // MARK: - Properties
+    private let searchBeerUseCase: SearchBeerUseCase
     
+    init(
+        searchBeerUseCase: SearchBeerUseCase
+    ) {
+        self.searchBeerUseCase = searchBeerUseCase
+        super.init()
+    }
 }
