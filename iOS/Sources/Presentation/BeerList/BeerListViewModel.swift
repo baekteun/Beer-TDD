d899aaa2f01abbb7f848df061fc5c724d8f9dd3e
baegteun <baegteun@gmail.com>
2022-03-23 15:25:19 +0900

    :sparkles: :: loadMoreItem Action

--- iOS/Sources/Presentation/BeerList/BeerListViewModel.swift
+++ iOS/Sources/Presentation/BeerList/BeerListViewModel.swift
@@ -4,6 +4,7 @@ import Service
 final class BeerListViewModel: BaseViewModel {
     // MARK: - Properties
     @Published var beerList: [Beer] = []
+    @Published var isListLoading: Bool = false
     private let fetchBeerListUseCase: FetchBeerListUseCase
     var page: Int = 1
     let size = 20
@@ -14,26 +15,45 @@ final class BeerListViewModel: BaseViewModel {
         self.fetchBeerListUseCase = fetchBeerListUseCase
         super.init()
     }
-     
+    
     enum Input {
         case onAppear
+        case loadMoreItem
     }
     
     func apply(_ input: Input) {
         switch input {
         case .onAppear:
             onAppear()
+        case .loadMoreItem:
+            loadMoreItem()
         }
     }
     
     // MARK: - Method
-    private func onAppear() {
+    
+}
+
+// MARK: - Method
+private extension BeerListViewModel {
+    func onAppear() {
         addCancellable(publisher: fetchBeerListUseCase.execute(page: page, size: size)) { [weak self] value in
             self?.beerList = value
+            self?.page += 1
         } onReceiveFailure: { [weak self] err in
             self?.isErrorOccurred = true
             self?.errorMessage = err.localizedDescription
         }
-
+    }
+    func loadMoreItem() {
+        isListLoading = true
+        addCancellable(publisher: fetchBeerListUseCase.execute(page: page, size: size)) { [weak self] value in
+            self?.beerList += value
+            self?.page += 1
+            self?.isListLoading = false
+        } onReceiveFailure: { [weak self] err in
+            self?.isErrorOccurred = true
+            self?.errorMessage = err.localizedDescription
+        }
     }
 }
--- iOS/Tests/ViewModel/BeetListViewModelSpec.swift
+++ iOS/Tests/ViewModel/BeetListViewModelSpec.swift
@@ -21,15 +21,27 @@ final class BeetListViewModelSpec: QuickSpec {
                 vm.apply(.onAppear)
             }
             context("데이터(Mock)가 fetch되면") {
-                
-                it("beerList의 카운터가 0보다 크다") {
+                it("beerList의 count가 0보다 크다") {
                     expect(vm.beerList.count).toEventually(beGreaterThan(0), timeout: .seconds(3))
                 }
                 it("beerList의 카운터가 size(20)만큼의 count를 가진다") {
                     expect(vm.beerList.count).toEventually(equal(20), timeout: .seconds(3))
                 }
             }
-            
+            context("화면에 마지막 아이템이 OnAppear된다면") {
+                beforeEach {
+                    vm.apply(.loadMoreItem)
+                }
+                it("아이템이 load될때 isListLoading이 true가 된다") {
+                    expect(vm.isListLoading).to(beTrue())
+                }
+                it("beerList의 카운터가 40이 된다.") {
+                    expect(vm.beerList.count).toEventually(equal(40), timeout: .seconds(5))
+                }
+                it("아이템이 load되고나서 isListLoading이 false가 된다") {
+                    expect(vm.isListLoading).toEventually(beFalse(), timeout: .seconds(5))
+                }
+            }
         }
     }
 }
