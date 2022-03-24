9ff49173a18fcdd779accc4fa6dbdd9569a94a10
baegteun <baegteun@gmail.com>
2022-03-24 10:56:03 +0900

    :sparkles: :: RandomBeer

--- iOS/Sources/Presentation/RandomBeer/RandomBeerView.swift
+++ iOS/Sources/Presentation/RandomBeer/RandomBeerView.swift
@@ -2,8 +2,31 @@
 import SwiftUI
 
 struct RandomBeerView: View {
+    @StateObject var viewModel = DependencyProvider.shared.container.resolve(RandomBeerViewModel.self)!
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                if let beer = viewModel.beer {
+                    BeerView(beer: beer)
+                }
+                Button {
+                    viewModel.apply(.randomButtonDidTap)
+                } label: {
+                    Text("Reset")
+                        .foregroundColor(.white)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 30)
+                        .background(.blue)
+                        .cornerRadius(20)
+                }
+            }
+            .onAppear {
+                viewModel.apply(.onAppear)
+            }
+            .navigationTitle(Text("RandomBeer"))
+            .navigationBarTitleDisplayMode(.large)
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
